5e4996c7bb805ece65c35fb2cd0b9c634d3262bd
lauji2k19 <lauji2k12@gmail.com>
2020-01-15 12:34:22 -0600

    Remove unused code in QRCodeHelper

--- app/src/main/java/com/example/googlesheetstest/QRCodeHelper.java
+++ app/src/main/java/com/example/googlesheetstest/QRCodeHelper.java
@@ -31,22 +31,6 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRCodeHelper {
-    public static void test() throws WriterException, IOException, NotFoundException {
-        String qrCodeData = "number=1732,initLine=1001,autoLower=1002,autoOuter=1003,autoInner=100,lower=100,outer=100,inner=100,rotation=100,position=100,park=100,hang=100,level=100,disableTime=100,notes=none";
-        String filePath = "code.png";
-        String charSet = "UTF-8";
-        Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-
-        createQRCode(qrCodeData);
-        System.out.println("QR code generated.");
-
-        //Dictionary<String, String> properties = readQRCode(filePath, charSet, hintMap);
-        //System.out.println(properties.get("initLine"));
-        //System.out.println(properties.get("autoLower"));
-        //System.out.println(properties.get("autoOuter"));
-    }
-
     public static Bitmap createQRCode(String qrCodeData) throws WriterException, IOException {
         QRCodeWriter writer = new QRCodeWriter();
         BitMatrix bitMatrix = writer.encode(qrCodeData, BarcodeFormat.QR_CODE, 512, 512);
@@ -60,20 +44,4 @@ public class QRCodeHelper {
         }
         return bmp;
     }
-
-    /*public static Dictionary<String, String> readQRCode(String filePath, String charset, Map hintMap)
-            throws FileNotFoundException, IOException, NotFoundException {
-        BinaryBitmap binaryBitmap = new BinaryBitmap(
-                new HybridBinarizer(new BufferedImageLuminanceSource(ImageIO.read(new FileInputStream(filePath)))));
-        Result qrCodeResult = new MultiFormatReader().decode(binaryBitmap, hintMap);
-        String[] propertyStrings = qrCodeResult.getText().split(",");
-        Dictionary<String, String> properties = new Hashtable<String, String>();
-
-        for (String property : propertyStrings) {
-            String[] splitProperty = property.split("=");
-            properties.put(splitProperty[0], splitProperty[1]);
-        }
-
-        return properties;
-    }*/
 }
