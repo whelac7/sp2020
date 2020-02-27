3be0bc320e2145dad4a161305c405d595296cb47
Ji Lau <lauji2k12@gmail.com>
2020-02-26 19:05:48 -0600

    Corrected commit msg: Restructure database to create separate tables for competitions, not teams

--- app/src/main/java/org/frc1732scoutingapp/helpers/JSONHelper.java
+++ app/src/main/java/org/frc1732scoutingapp/helpers/JSONHelper.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+
 import org.frc1732scoutingapp.models.MatchResult;
 
 public class JSONHelper {
